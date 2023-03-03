784916a0c4407854cb25fee1c04217c21e904438
Ken Okamoto <ken.okamoto530@gmail.com>
2023-03-03 23:46:33 +0800

    sall

--- react-website/src/components/Header.js
+++ react-website/src/components/Header.js
@@ -18,15 +18,48 @@ const Header = () => {
       </header>
       <header className='header-upper py-3'>
         <div className='container-xxl'>
-          <div className='row'>
+          <div className='row align-items-center'>
             <div className='col-2'>
               <h2><Link className='text-white'>Mang Wrap</Link></h2>
             </div>
-            <div className='col-5'><div className=  "input-group mb-3">
-              <input type="text" className="form-control" placeholder="Search Product Here..." aria-label="Search Product Here..." aria-describedby="basic-addon2"/>
-              <span className="input-group-text" id="basic-addon2"><BsSearch/></span>
+            <div className='col-5'><div className=  "input-group">
+              <input type="text" className="form-control py-2" placeholder="Search Product Here..." aria-label="Search Product Here..." aria-describedby="basic-addon2"/>
+              <span className="input-group-text p-3" id="basic-addon2"><BsSearch className='fs-6'/></span>
             </div></div>
-            <div className='col-5'></div>
+            <div className='col-5'>
+              <div className='header-upper-links d-flex justify-content-end gap-15'>
+                {/* <div>
+                  <Link className='d-flex align-items-center gap-10 text-white'>
+                    <img src='/images/compare.svg' alt='compare'/>
+                    <p>Compare <br/> 
+                    Products</p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className='d-flex align-items-center gap-10 text-white'>
+                  <img src='/images/wishlist.svg' alt='wishlist'/>
+                    <p>Favorite <br/> 
+                     WishList</p>
+                  </Link>
+                </div> */}
+                <div>
+                  <Link className='d-flex align-items-center gap-10 text-white'>
+                  <img src='/images/user.svg' alt='user'/>
+                    <p className='mb-0'>Log in <br/> 
+                    My Account</p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className='d-flex align-items-center gap-10 text-white'>
+                  <img src='/images/cart.svg' alt='cart'/>
+                    <div className='d-flex flex-column gap-10'>
+                      <span className='badge bg-white text-dark'>0</span>
+                      <p className='mb-0'>$ 500</p>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </header>
